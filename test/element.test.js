0ec28303ab3d832bc78a76256b4c79cf55ac601e
Subrato Sarkar <subratosarkar29@gmail.com>
2020-05-22 21:16:04 -0400

    Update element.test.js

--- test/element.test.js
+++ test/element.test.js
@@ -1,22 +1,22 @@
-// const internetPage = require('../pages/internet.page')
+const internetPage = require('../pages/internet.page')
 
-// describe("Interacting with element", function() {
-// 	it("Get text for element", () => {
-// 		browser.url('/')
-// 		let text = $('h2').getText()
-// 		console.log("The text is: "+text)
-// 		internetPage.getLiText()
-// 	})
+describe("Interacting with element", function() {
+	it("Get text for element", () => {
+		browser.url('/')
+		let text = $('h2').getText()
+		console.log("The text is: "+text)
+		internetPage.getLiText()
+	})
 
-// 	it("Is footer displayed", () => {
-// 		console.log("Display check: "+internetPage.pageFooter.isDisplayed());
-// 	})
+	it("Is footer displayed", () => {
+		console.log("Display check: "+internetPage.pageFooter.isDisplayed());
+	})
 
-// 	it("Does the header exist", () => {
-// 		console.log("Existance check: "+internetPage.pageHeader.isExisting());
-// 	})
+	it("Does the header exist", () => {
+		console.log("Existance check: "+internetPage.pageHeader.isExisting());
+	})
 
-// 	it("Clicking First link", () => {
-// 		internetPage.clickFirstLink();
-// 	})
-// })
+	it("Clicking First link", () => {
+		internetPage.clickFirstLink();
+	})
+})
